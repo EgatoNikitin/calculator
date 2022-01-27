dbdfe8702a4d987675aca558962630cf7eb4d0a3
EgatoNikitin <ve4er_top@mail.ru>
2022-01-27 21:08:07 +0300

    js

--- js.js
+++ js.js
@@ -1,1 +1,24 @@
-const numbers = document.querySelectorAll("main-button")
+const numbers = document.querySelectorAll(".numbers")
+const operators = document.querySelectorAll(".operators")
+const result = document.querySelector(".result")
+const clear = document.querySelector(".clear")
+const clearAll = document.querySelector(".clear-all")
+const input = document.querySelector(".input")
+
+
+console.log(numbers)
+
+
+numbers.forEach((number) => {
+    number.addEventListener('click', (e) => {
+        numberPress(e.target.textContent)
+    })
+})
+
+function numberPress(number) {
+    if (input.value === "") {
+        input.value = number;
+    } else {
+        input.value += number;
+    }
+}
